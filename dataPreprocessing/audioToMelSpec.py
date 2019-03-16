40100bba19c72c1c98c6c47845a9364e67924d79
Nick Blaskey <nicholas.blaskey1@marist.edu>
2019-03-16 18:01:42 -0400

    Made preprocessing work for all files in directory

--- dataPreprocessing/audioToMelSpec.py
+++ dataPreprocessing/audioToMelSpec.py
@@ -1,12 +1,12 @@
 import os
 import matplotlib
+matplotlib.use('Agg')           # No pictures displayed 
 import pylab
 import librosa
 import librosa.display
 import numpy as np
 from pydub import AudioSegment
 
-matplotlib.use('Agg')           # No pictures displayed 
 PATH_TO_IMPORT = "splitAudio/"  # Where the split audio is   
 PATH_TO_EXPORT = "melSpecs/"    # Where to save the png
 
@@ -17,20 +17,23 @@ PATH_TO_EXPORT = "melSpecs/"    # Where to save the png
 #
 # parameters:
 #   file_name: The name of the file to convert
-#   output_name: The name of what the image should be saved as
 #
 # return: none
-def audio_to_png(file_name, image_name):    
-    sig, fs = librosa.load('rawAudio/test.wav')   
-    save_path = 'testwav.png'
+def audio_to_png(file_name):    
+    sig, fs = librosa.load(PATH_TO_IMPORT + file_name + '.wav')   
+    save_path = PATH_TO_EXPORT + file_name + '.png'
     
     pylab.axis('off') # no axis
     pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
     S = librosa.feature.melspectrogram(y=sig, sr=fs)
     librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-    pylab.savefig(output_name, bbox_inches=None, pad_inches=0)
+    pylab.savefig(save_path, bbox_inches=None, pad_inches=0)
     pylab.close()
 
+def main():
+    for file_name in os.listdir(PATH_TO_IMPORT):
+        audio_to_png(file_name[:-4])
 
-        
+if __name__ == "__main__":
+    main()
 
--- dataPreprocessing/splitAudio.py
+++ dataPreprocessing/splitAudio.py
@@ -6,11 +6,12 @@
 # starting with 0 for open E to 84 for the 20th fret on the high G
 # string. The file output will be named fretNumber#sampleNumber.mp3
 
-
+from os import listdir
 from pydub import AudioSegment
-SONG_TEMPO = 240              # In bpm
-LENGTH_OF_PLAYING = 5         # In seconds
-PATH_TO_IMPORT = "rawAudio/"  # Where the raw audio is   
+
+SONG_TEMPO = 240                # In bpm
+LENGTH_OF_PLAYING = 5           # In seconds
+PATH_TO_IMPORT = "rawAudio/"    # Where the raw audio is   
 PATH_TO_EXPORT = "splitAudio/"  # Where to save the audio
 
 
@@ -24,19 +25,21 @@ PATH_TO_EXPORT = "splitAudio/"  # Where to save the audio
 #
 # return: none
 def split_one_file(file_name):        
-    audio = AudioSegment.from_mp3(PATH_TO_IMPORT + file_name + ".mp3")
+    audio = AudioSegment.from_mp3(PATH_TO_IMPORT + file_name + ".wav")
 
     note_length = (int) (60 / SONG_TEMPO * 1000)
     # split sound in 5-second slices and export
     for i, chunk in enumerate(audio[::note_length]):
-      with open(PATH_TO_EXPORT + file_name + "#%i.mp3" % i, "wb") as f:
-        chunk.export(f, format="mp3")
+      with open(PATH_TO_EXPORT + file_name + "#%i.wav" % i, "wb") as f:
+        chunk.export(f, format="wav")
 
 def main():
-    split_one_file("test")
-
+    # Split all files in the directory
+    for file_name in listdir(PATH_TO_IMPORT):
+        split_one_file(file_name[:-4])
+        
 if __name__ == "__main__":
-    split_one_file("test")
+    main()
 
 
 
